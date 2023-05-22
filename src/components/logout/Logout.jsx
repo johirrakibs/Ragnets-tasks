98358c777c3b87cb27427cc2611ea61904cd4a69
johirrakibs <johir.rakib59@gmail.com>
2023-05-22 23:09:33 +0600

    ADDD

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const LogoutPage = () => {
     useEffect(() => {
-        window.location.href = '/login';
+        window.location.href = 'https://ragnets-tasks.vercel.app/login';
     }, []);
 
     return <div>Logging out...</div>;
